264bc70af8093c86ee32cc685ffaf51ecc474521
Priyansh   Dhiman <Priyansh.dhiman25@gmail.com>
2025-10-05 14:38:06 -0500

    Initial Commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
     public static void main (String [] args) {
 
+        Car car1 = new Car();
+        Car car2 = new Car();
+
+
+        car1.drive();
+        car1.brake();
+        car2.drive();
+        car2.brake();
+
 
     }
 }
