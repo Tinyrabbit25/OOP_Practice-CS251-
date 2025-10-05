4610f436d81b78f359387716568c4dca7dc16c96
Priyansh   Dhiman <Priyansh.dhiman25@gmail.com>
2025-10-05 14:37:55 -0500

    Initial Commit

--- src/Car.java
+++ src/Car.java
@@ -1,28 +1,27 @@
 public class Car {
 
-        //Attributes
-        String make = "Honda";
-        String model = "Sedan";
-        int year = 2025;
-        double price = 58000.89;
-        boolean is_running = false;
+    String make = "Ford";
+    String model = "Mustang";
+    int year = 2025;
+    double price = 1999.9;
+    boolean is_running = false;
 
+    void start(){
+        is_running = true;
+        System.out.println("You start the engine");
+    }
 
-        void start(){
-            is_running = true;
-            System.out.println("You start the engine");
-        }
+    void stop(){
+        is_running = false;
+        System.out.println("You stop the engine");
+    }
 
-        void stop(){
-            is_running = false;
-            System.out.println("You stop the engine");
-        }
+    void drive(){
+        System.out.println("You drive the " + model);
+    }
 
-        void drive(){
-            System.out.println("You drive the " + model);
-        }
+    void brake(){
+        System.out.println("You brake the " + model);
+    }
 
-        void brake(){
-            System.out.println("You brake the " + model);
-        }
 }
