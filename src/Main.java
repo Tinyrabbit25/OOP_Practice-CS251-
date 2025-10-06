bf0c93d36598e914ce9c4a735c8fdb349b446651
Priyansh   Dhiman <Priyansh.dhiman25@gmail.com>
2025-10-06 14:53:14 -0500

    Learned ToString

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,7 @@
 public class Main {
     public static void main (String [] args) {
 
-        Car car1 = new Car("Ford","Mustang",2025, "red");
-        Car car2 = new Car("Audi","S2",2025, "black");
-
-
-        System.out.println(car1);
-        System.out.println(car2);
-        Car.track();
+        
 
     }
 }
